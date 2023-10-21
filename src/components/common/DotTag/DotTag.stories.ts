cfc466e18d146945b4613815ef50c91172fd8cc2
HongGunWoo <foxrain.gg@gmail.com>
2023-10-21 16:25:43 +0900

    ✨ Feat: DotTag 컴포넌트 추가

---
+++ src/components/common/DotTag/DotTag.stories.ts
@@ -0,0 +1,21 @@
+import { IconProps } from '@chakra-ui/react';
+import { Meta, StoryObj } from '@storybook/react';
+import DotTag from './DotTag';
+
+const meta: Meta<typeof DotTag> = {
+  title: 'Components/Common/DotTag',
+  component: DotTag,
+  tags: ['autodocs'],
+  argTypes: {
+    color: {
+      control: { type: 'color' },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof DotTag>;
+
+export const Default: Story = {
+  args: { size: 'md' },
+};
--- src/theme/components/index.ts
+++
@@ -1,8 +0,0 @@
-import { Button, DotTag } from './common';
-
-const components = {
-  Button,
-  DotTag,
-};
-
-export default components;
