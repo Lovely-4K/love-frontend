2b538a2ec0782a127e70050cea705d799ef97300
cloud0406 <cloud0406@naver.com>
2023-10-23 18:19:12 +0900

    📝 Chore: tailwind config 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,56 @@
+import plugin from 'tailwindcss/plugin';
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: { base: '#59473C', darken: '#311B0E', lighten: '#8C6F5E' },
+        active: {
+          base: '#FF7A00',
+          darken: '#D76700',
+          lighten: '#FFC998',
+          lightest: '#FFE6CE',
+        },
+        sub: {
+          blue: '#487DAD',
+          sky: '#CCF7FA',
+          red: '#FF3D00',
+          lime: '#40DF32',
+          green: '#349E2C',
+        },
+        gray: {
+          100: '#F1F1F1',
+          200: '#DCDDDE',
+          300: '#8A8A8A',
+          400: '#5F5F5F',
+          500: '#313131',
+          600: '#B1B1B1',
+        },
+        white: '#FFFFFF',
+        black: '#000000',
+      },
+      fontFamily: { OAGothic: ['OAGothic'] },
+      screens: {
+        mobile: {},
+        tablet: {},
+        desktop: {},
+        cs: '0px',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        '.no-scrollbar::-webkit-scrollbar': {
+          display: 'none',
+        },
+        '.no-scrollbar': {
+          scrollbarWidth: 'none',
+          '-ms-overflow-style': 'none',
+        },
+      });
+    }),
+  ],
 };
