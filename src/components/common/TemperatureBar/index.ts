acb5047cb28c871563377388d94359e948f40f59
cloud0406 <32586926+cloud0406@users.noreply.github.com>
2023-10-23 15:40:39 +0900

    ✨ Feat: 온도바 컴포넌트 추가 (#9)
    Co-authored-by: Suyeon <80307321+suyeon1218@users.noreply.github.com>

---
+++ src/components/common/TemperatureBar/index.ts
@@ -0,0 +1,1 @@
+export { default as TemperatureBar } from './TemperatureBar';
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
 export * from './DotTag';
+export * from './TemperatureBar';
 export * from './CustomAvatar';
 export * from './MessageBox';
