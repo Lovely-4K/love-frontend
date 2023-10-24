ca2d4243e706c7e5f41b228f35615d93761f6525
Suyeon <msjgr2801@gmail.com>
2023-10-24 18:51:24 +0900

    ✨ Feat: 폰트 타입 추가

---
+++ src/theme/font.ts
@@ -0,0 +1,16 @@
+const font = {
+  '.font-title': {
+    'font-size': '20px',
+  },
+  '.font-large': {
+    'font-size': '16px',
+  },
+  '.font-medium': {
+    'font-size': '14px',
+  },
+  'font-small': {
+    'font-size': '12px',
+  },
+};
+
+export default font;
