cee2c3320c14da6061a174a42c41856637c02bbe
HGW <45515388+HongGunWoo@users.noreply.github.com>
2023-10-24 14:55:41 +0900

    ✨ Feat: Carousel 컴포넌트 추가 (#22)
    * ✨ Feat: Carousel 컴포넌트 추가
    
    * 🔨 Refactor: 구조분해할당 적용

---
+++ src/components/domain/Carousel/Carousel.tsx
@@ -0,0 +1,57 @@
+import { memo, useRef, useState } from 'react';
+
+interface CarouselProps {
+  pictures: string[];
+}
+
+const Carousel = memo(({ pictures }: CarouselProps) => {
+  const carouselRef = useRef<HTMLDivElement>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleScroll = () => {
+    if (carouselRef.current === null) return;
+
+    const { scrollLeft, clientWidth } = carouselRef.current;
+
+    const index = Math.round(scrollLeft / clientWidth);
+    setActiveIndex(index);
+  };
+
+  const handleButtonClick = (index: number) => {
+    if (carouselRef.current === null) return;
+
+    carouselRef.current.scrollTo({
+      left: index * carouselRef.current.clientWidth,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <div className="relative">
+      <div
+        ref={carouselRef}
+        className="carousel w-full rounded-[0.625rem]"
+        onScroll={handleScroll}
+      >
+        {pictures.map((picture, index) => (
+          <div key={index} id={`item${index}`} className="carousel-item w-full">
+            <img src={picture} className="w-full" />
+          </div>
+        ))}
+      </div>
+      <div className="absolute bottom-5 left-1/2 flex w-full -translate-x-1/2 justify-center gap-1 py-2">
+        {pictures.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => handleButtonClick(index)}
+            className={`h-3 w-3 rounded-full ${
+              index === activeIndex ? 'bg-black' : 'bg-gray-500'
+            }`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+});
+
+export default Carousel;
