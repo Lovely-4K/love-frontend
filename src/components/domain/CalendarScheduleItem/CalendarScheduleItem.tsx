3155ddb04b5fe86b4f533ddf1b59a255227a863c
Suyeon <msjgr2801@gmail.com>
2023-10-25 18:53:50 +0900

    ✨ Feat: CalendarScheduleItem 도메인 컴포넌트 초기 구현

---
+++ src/components/domain/CalendarScheduleItem/CalendarScheduleItem.tsx
@@ -0,0 +1,5 @@
+const CalendarScheduleItem = () => {
+  return <div></div>;
+};
+
+export default CalendarScheduleItem;
---
+++ src/components/domain/CalendarScheduleItem/index.ts
@@ -0,0 +1,1 @@
+export { default as CalendarScheduleItem } from './CalendarScheduleItem';
