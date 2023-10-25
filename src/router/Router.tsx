2344eb1df62a8fe3b6b1255497e1093aac5d2d38
Suyeon <msjgr2801@gmail.com>
2023-10-25 16:18:44 +0900

    📝 Chore: Router 코드 불필요한 코드 삭제

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './Layout';
-import Test from '~/pages/test';
 
 // todo: 인증 사용자가 아니면 PrivateRoute 로 이동, 인증 사용자일 시 PublicRouter 로 이동
 
@@ -8,9 +7,7 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route element={<Test />} path="/" />
-        </Route>
+        <Route element={<Layout />}></Route>
       </Routes>
     </BrowserRouter>
   );
