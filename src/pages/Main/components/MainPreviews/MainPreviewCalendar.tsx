ea2306daa1279528ea5c23b95bfa5bcf85b1c3c1
Suyeon <msjgr2801@gmail.com>
2023-10-25 18:53:25 +0900

    ✨ Feat: MainPreviewCalendar 컴포넌트 초기 구현

---
+++ src/pages/Main/components/MainPreviews/MainPreviewCalendar.tsx
@@ -0,0 +1,12 @@
+const MainPreivewCalendar = () => {
+  return (
+    <>
+      <CalendarScheduleItem />
+      <CalendarScheduleItem />
+      <CalendarScheduleItem />
+      <CalendarScheduleItem />
+    </>
+  );
+};
+
+export default MainPreivewCalendar;
