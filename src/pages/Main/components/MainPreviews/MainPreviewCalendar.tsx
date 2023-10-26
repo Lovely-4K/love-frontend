35baed07ac9d9f9bebb529d78f7ffd5f9d48e003
Suyeon <msjgr2801@gmail.com>
2023-10-26 16:16:36 +0900

    💄 Style: 모바일에서 우측 요소 가려지는 문제 해결

--- src/pages/Main/components/MainPreviews/MainPreviewCalendar.tsx
+++ src/pages/Main/components/MainPreviews/MainPreviewCalendar.tsx
@@ -2,7 +2,7 @@ import { CalendarScheduleItem } from '~/components/domain';
 
 const MainPreviewCalendar = () => {
   return (
-    <div className="scrollbar-hide flex w-full overflow-x-scroll lg:flex-col lg:overflow-x-hidden lg:overflow-y-scroll">
+    <div className="scrollbar-hide flex w-full overflow-x-scroll lg:flex-col lg:overflow-x-hidden lg:overflow-y-scroll [&>*:last-child]:relative [&>*:last-child]:after:absolute [&>*:last-child]:after:-right-5 [&>*:last-child]:after:block [&>*:last-child]:after:h-5 [&>*:last-child]:after:w-5">
       <CalendarScheduleItem
         customColor={'blue'}
         date={'2023년 11월 06일'}
