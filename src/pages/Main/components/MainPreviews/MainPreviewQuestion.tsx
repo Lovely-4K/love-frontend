eb91948cc94d9da272ff8c5f6c4b0830aae067ea
Suyeon <msjgr2801@gmail.com>
2023-10-26 14:22:59 +0900

    ✨ Feat: MainPreviewQuestion 초기 구현

---
+++ src/pages/Main/components/MainPreviews/MainPreviewQuestion.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+const MainPreivewQuestion = () => {
+  return (
+    <div>
+      <div>
+        <img alt="질문자 사진" />
+      </div>
+      <div>
+        <div>세미콜론 쓰기 vs 안 쓰기</div>
+        <Link to="/question">
+          <div>답변하러 가기</div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default MainPreivewQuestion;
