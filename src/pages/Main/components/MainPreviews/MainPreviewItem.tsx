c9b12ebe0f39b8d17e146552f5dc14cb3201559c
Suyeon <msjgr2801@gmail.com>
2023-10-26 18:17:27 +0900

    💄 Styles: MainPreviewItem 스타일 수정

--- src/pages/Main/components/MainPreviews/MainPreviewItem.tsx
+++ src/pages/Main/components/MainPreviews/MainPreviewItem.tsx
@@ -13,9 +13,9 @@ const MainPreviewItem = ({
   content,
 }: MainPreviewItemProps) => {
   return (
-    <div className="scrollbar-hide mx-3 flex-grow rounded-xl px-4 py-3 lg:h-[28.125rem] lg:w-[18.75rem] lg:border lg:border-solid lg:border-grey-200">
+    <div className="mx-3 flex w-full flex-grow flex-col rounded-xl px-4 py-3 lg:h-[28.125rem] lg:w-[18.75rem] lg:border lg:border-solid lg:border-grey-200">
       <Link to={pageLink}>
-        <div className="font-title my-2 font-bold">{title}</div>
+        <div className="h-15 font-title my-2 w-full font-bold">{title}</div>
       </Link>
       {content}
     </div>
