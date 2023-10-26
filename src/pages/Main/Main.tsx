648ae907eb31bcfa251d454a75915fd8ff2cb0f6
Suyeon <msjgr2801@gmail.com>
2023-10-26 18:15:33 +0900

    💄 Styles: mainpage 스타일 수정

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,7 +2,7 @@ import { MainPreviews } from './components';
 
 const MainPage = () => {
   return (
-    <div className="flex h-full w-full flex-shrink flex-col justify-center lg:items-center">
+    <div className="flex h-full w-full flex-shrink flex-col justify-center overflow-y-auto overflow-x-hidden lg:items-center">
       <MainPreviews />
     </div>
   );
