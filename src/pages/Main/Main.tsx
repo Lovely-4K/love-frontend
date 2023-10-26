0ae702a31be8b30d9a1a8d197190f9452d32b9f9
cloud0406 <cloud0406@naver.com>
2023-10-26 14:22:50 +0900

    ✨ Feat: 메인 페이지 top-content(버튼, 프로필, d-day) 레이아웃 설정

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,5 +1,44 @@
+import { IconHeart } from '~/assets/icons';
+
 const MainPage = () => {
-  return <></>;
+  return (
+    <>
+      <div className="mb-5 flex justify-end gap-[0.625rem]">
+        <button className="btn-medium rounded-xl border border-grey-200 bg-base-white font-bold text-grey-400">
+          프로필 수정
+        </button>
+        <button className="btn-medium rounded-xl border border-grey-200 bg-base-white font-bold text-grey-400">
+          디데이 수정
+        </button>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-3">
+        <div className="relative flex gap-2">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <div className="avatar">
+              <div className="avatar-extra-large rounded-full">
+                <img src="https://source.unsplash.com/random/" />
+              </div>
+            </div>
+            <span className="font-title font-bold">정</span>
+            <span className="font-medium text-grey-500">ISFJ</span>
+          </div>
+          <IconHeart
+            className={`absolute left-[8rem] top-[3rem] z-10  h-[3.1255rem] w-[3.125rem] fill-base-primary stroke-base-primary`}
+          />
+          <div className="flex flex-col items-center justify-center gap-2">
+            <div className="avatar">
+              <div className="avatar-extra-large rounded-full">
+                <img src="https://source.unsplash.com/random/" />
+              </div>
+            </div>
+            <span className="font-title font-bold">정</span>
+            <span className="font-medium text-grey-500">ISFJ</span>
+          </div>
+        </div>
+        <span className="text-3xl text-base-primary">D+123</span>
+      </div>
+    </>
+  );
 };
 
 export default MainPage;
