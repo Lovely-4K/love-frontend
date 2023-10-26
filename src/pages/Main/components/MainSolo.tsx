51f065b5783b8464a76e7cfa0a67bf4b12b16d10
cloud0406 <cloud0406@naver.com>
2023-10-26 19:10:05 +0900

    ✨ Feat: 초대 링크 렌더링 처리

--- src/pages/Main/components/MainSolo.tsx
+++ src/pages/Main/components/MainSolo.tsx
@@ -1,7 +1,28 @@
+import { useState } from 'react';
 import { IconCopyLink } from '~/assets/icons';
 import { MainProfile } from '~/pages/Main/components';
 
 const MainSolo = () => {
+  const [linkToggle, setLinkToggle] = useState(false);
+
+  const handleClick = () => {
+    setLinkToggle(true);
+  };
+
+  const linkContent = linkToggle && (
+    <div className="join flex w-full border border-grey-200">
+      <input
+        className="input join-item grow focus:outline-none"
+        placeholder="초대 링크를 발급받으세요!"
+        content="hello"
+        readOnly
+      ></input>
+      <button className="btn join-item">
+        <IconCopyLink className="h-7 w-7" />
+      </button>
+    </div>
+  );
+
   return (
     <>
       <div className="flex flex-col items-center justify-center gap-14 px-4 pt-20">
@@ -10,22 +31,15 @@ const MainSolo = () => {
           mbti="ISFJ"
           picture="https://source.unsplash.com/random/"
         />
-        <div className="flex min-w-[21rem] max-w-[27rem] flex-col items-center justify-center gap-6 rounded-3xl border border-grey-200 px-6 py-7 ">
+        <div className="flex min-w-[21rem] max-w-[27rem] flex-col items-center justify-center gap-6 rounded-3xl border border-grey-200 px-6 py-7">
           <span className="font-title break-keep">
             상대방을 초대하고 우이삭 컨텐츠를 이용해보세요!
           </span>
-          <div className="join flex w-full border border-grey-200">
-            <input
-              className="input join-item grow focus:outline-none"
-              placeholder="초대 링크를 발급받으세요!"
-              content="hello"
-              readOnly
-            ></input>
-            <button className="btn join-item">
-              <IconCopyLink className="h-7 w-7" />
-            </button>
-          </div>
-          <button className="btn-extra-large w-full rounded-xl bg-base-primary text-base-white">
+          {linkContent}
+          <button
+            onClick={handleClick}
+            className="btn-extra-large w-full rounded-xl bg-base-primary text-base-white"
+          >
             초대 링크 발급하기
           </button>
         </div>
