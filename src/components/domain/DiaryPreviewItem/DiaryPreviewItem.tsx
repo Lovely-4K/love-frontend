0071ffd02914cca1a65e3b330763e22125d05905
Suyeon <msjgr2801@gmail.com>
2023-10-26 17:45:53 +0900

    ✨ DiaryPreviewItem 구현
    - hover 시 content 보여주기

---
+++ src/components/domain/DiaryPreviewItem/DiaryPreviewItem.tsx
@@ -0,0 +1,23 @@
+interface DiaryPreviewItemProps {
+  date: string;
+  location: string;
+  imgSrc: string;
+}
+
+const DiaryPreviewItem = ({
+  date,
+  location,
+  imgSrc,
+}: DiaryPreviewItemProps) => {
+  return (
+    <div className="relative h-32 w-32 cursor-pointer items-center justify-center [&>div]:hover:visible">
+      <div className="font-medium invisible absolute left-[50%] top-[50%] z-20 flex h-full  w-full translate-x-[-50%] translate-y-[-50%] flex-col items-center justify-center rounded-xl bg-base-deem text-base-white">
+        <div>{date}</div>
+        <div>{location}</div>
+      </div>
+      <img className="image-square" src={imgSrc} alt="다이어리 미리보기" />
+    </div>
+  );
+};
+
+export default DiaryPreviewItem;
--- src/components/domain/index.ts
+++ src/components/domain/index.ts
@@ -2,3 +2,4 @@ export * from './Carousel';
 export * from './Rating';
 export * from './Footer';
 export * from './CalendarScheduleItem';
+export * from './DiaryPreviewItem';
