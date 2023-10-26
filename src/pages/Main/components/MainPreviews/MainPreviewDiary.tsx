602acdbf97c4e57f2e978b4763cfa517ffaeaa36
Suyeon <msjgr2801@gmail.com>
2023-10-26 14:20:31 +0900

    ✨ Feat: MainPreivewDiary 초기 구현

---
+++ src/pages/Main/components/MainPreviews/MainPreviewDiary.tsx
@@ -0,0 +1,12 @@
+const MainPreviewDiary = () => {
+  return (
+    <div>
+      <div>
+        <div>2023 년 09월 05일 카페 녹다</div>
+        <img className="img-square" src="" />
+      </div>
+    </div>
+  );
+};
+
+export default MainPreviewDiary;
