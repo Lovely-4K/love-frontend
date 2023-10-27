ab02bbc68de4d81731a36fa61e18ea81fe4af5c8
Suyeon <msjgr2801@gmail.com>
2023-10-27 14:40:23 +0900

    ✨ Feat: MainPreviewQuestion 컴포넌트 구현

--- src/pages/Main/components/MainPreviews/MainPreviewQuestion.tsx
+++ src/pages/Main/components/MainPreviews/MainPreviewQuestion.tsx
@@ -2,14 +2,18 @@ import { Link } from 'react-router-dom';
 
 const MainPreivewQuestion = () => {
   return (
-    <div>
-      <div>
-        <img alt="질문자 사진" />
+    <div className="flex h-full w-full items-center md:flex-col md:justify-center">
+      <div className="m-4 flex h-[40%] items-center justify-center">
+        <img
+          className="w-24 rounded-full"
+          src="https://picsum.photos/200"
+          alt="질문자 사진"
+        />
       </div>
-      <div>
+      <div className="flex h-[40%] w-[70%] flex-col items-center">
         <div>세미콜론 쓰기 vs 안 쓰기</div>
         <Link to="/question">
-          <div>답변하러 가기 →</div>
+          <div className="font-small text-grey-400">답변하러 가기 →</div>
         </Link>
       </div>
     </div>
