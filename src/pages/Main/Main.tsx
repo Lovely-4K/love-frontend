7437e5f0f2959d00ef0dd034d7ad8989a762f7f5
Suyeon <msjgr2801@gmail.com>
2023-10-27 17:28:16 +0900

    📝 Chore: MainPage 스타일 분리

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,10 +1,20 @@
+import styled from '@emotion/styled';
 import { MainPreviews } from './components';
+
+const MainPageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  flex-shrink: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
 const MainPage = () => {
   return (
-    <div className="flex h-full w-full flex-shrink flex-col justify-center overflow-y-auto overflow-x-hidden md:items-center">
+    <MainPageContainer className="md:items-center">
       <MainPreviews />
-    </div>
+    </MainPageContainer>
   );
 };
 
--- src/pages/Main/components/index.ts
+++ src/pages/Main/components/index.ts
@@ -1,4 +1,4 @@
 export { default as MainProfile } from './MainProfile';
 export { default as MainCouple } from './MainCouple';
 export { default as MainSolo } from './MainSolo';
-export { default as MainPreviews } from './MainPreviews';
+export * from './MainPreviews';
