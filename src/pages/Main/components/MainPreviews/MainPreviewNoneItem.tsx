15c3fec582cbb2907a55df6b5cbc24e16b7613c4
Suyeon <msjgr2801@gmail.com>
2023-10-29 18:56:05 +0900

    ✨ Feat: NoneItem 컴포넌트 구현

---
+++ src/pages/Main/components/MainPreviews/MainPreviewNoneItem.tsx
@@ -0,0 +1,15 @@
+interface MainPreviewNoneItemProps {
+  itemName: string;
+}
+
+const MainPreviewNoneItem = ({ itemName }: MainPreviewNoneItemProps) => {
+  const placeholder = `아직 등록된 '${itemName}' 이(가) 없어요`;
+
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <span className="text-grey-400">{placeholder}</span>
+    </div>
+  );
+};
+
+export default MainPreviewNoneItem;
