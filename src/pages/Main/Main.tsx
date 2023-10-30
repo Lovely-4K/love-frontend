0bdd77f322d9cd45e14f6d3516678b7ee2de1268
HongGunWoo <foxrain.gg@gmail.com>
2023-10-30 13:52:56 +0900

    📝 Chore: Main 컴포넌트 import 수정

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,10 +1,6 @@
-import { MainProfileModal } from './components/MainProfileModal';
-
 const MainPage = () => {
   return (
-    <div className="flex h-full w-full flex-shrink flex-col justify-center overflow-y-auto overflow-x-hidden md:items-center">
-      <MainProfileModal />
-    </div>
+    <div className="flex h-full w-full flex-shrink flex-col justify-center overflow-y-auto overflow-x-hidden md:items-center"></div>
   );
 };
 
