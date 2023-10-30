d42792ecd8e00187694d73683ac64a80d11c0d8b
HongGunWoo <foxrain.gg@gmail.com>
2023-10-31 01:03:23 +0900

    📝 Chore: 파일 이름 수정

---
+++ src/pages/Main/components/MainProfileModal/MBTIButton.tsx
@@ -0,0 +1,49 @@
+import { memo } from 'react';
+
+interface MBTIButtonProps {
+  position: 'left' | 'center' | 'right';
+  topItem: 'E' | 'N' | 'F' | 'J';
+  bottomItem: 'I' | 'S' | 'T' | 'P';
+  selected?: string;
+}
+
+const topItemStyle = {
+  left: 'rounded-ss-lg',
+  center: '',
+  right: 'rounded-se-lg',
+};
+
+const bottomItemStyle = {
+  left: 'rounded-es-lg',
+  center: '',
+  right: 'rounded-ee-lg',
+};
+
+const MBTIButton = memo(
+  ({ position, topItem, bottomItem, selected }: MBTIButtonProps) => {
+    const topItemTextColor =
+      selected === topItem ? 'text-base-primary' : 'text-grey-400';
+
+    const bottomItemTextColor =
+      selected === bottomItem ? 'text-base-primary' : 'text-grey-400';
+
+    return (
+      <div className={`flex flex-col`}>
+        <button
+          className={`h-8 w-8 bg-grey-200 ${topItemTextColor} ${topItemStyle[position]}`}
+          value={topItem}
+        >
+          {topItem}
+        </button>
+        <button
+          className={`h-8 w-8 bg-grey-200 ${bottomItemTextColor} ${bottomItemStyle[position]}`}
+          value={bottomItem}
+        >
+          {bottomItem}
+        </button>
+      </div>
+    );
+  },
+);
+
+export default MBTIButton;
