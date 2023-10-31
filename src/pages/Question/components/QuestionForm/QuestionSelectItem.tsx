08efdab0085fb7d59f30c6756614a17068856fec
Suyeon <msjgr2801@gmail.com>
2023-10-31 15:20:07 +0900

    ✨ Feat: ChatItem 선택 시 스타일 변경 기능 추가

--- src/pages/Question/components/QuestionForm/QuestionSelectItem.tsx
+++ src/pages/Question/components/QuestionForm/QuestionSelectItem.tsx
@@ -1,10 +1,17 @@
 interface QuestionSelectItemProps {
   answer: string;
+  active: boolean;
 }
 
-const QuestionSelectItem = ({ answer }: QuestionSelectItemProps) => {
+const QuestionSelectItem = ({ answer, active }: QuestionSelectItemProps) => {
+  const activeStyle = active
+    ? 'border-none bg-primary text-base-white hover:bg-base-secondary'
+    : 'border border-grey-200 py-3 hover:bg-grey-100';
+
   return (
-    <div className="flex flex-grow cursor-pointer items-center justify-center rounded-xl border border-grey-200 py-3 hover:bg-grey-100">
+    <div
+      className={`flex flex-grow cursor-pointer items-center justify-center rounded-xl ${activeStyle}`}
+    >
       {answer}
     </div>
   );
