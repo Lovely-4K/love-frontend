f4c40aee92fd07295cd3e12d25eb7b30421a958f
Suyeon <msjgr2801@gmail.com>
2023-10-31 16:53:50 +0900

    📝 Chore: History prefix 로 이름 변경

---
+++ src/pages/QuestionHistory/components/HistoryHeader.tsx
@@ -0,0 +1,3 @@
+const HistoryHeader = () => {};
+
+export default HistoryHeader;
---
+++ src/pages/QuestionHistory/components/HistoryList/HistoryList.tsx
@@ -0,0 +1,14 @@
+import HistoryItem from './HistoryItem';
+
+const HistoryList = () => {
+  return (
+    <div>
+      <HistoryItem questionTitle={'둘 중 하나에 취직한다면?'} />
+      <HistoryItem questionTitle={'둘 중 하나에 취직한다면?'} />
+      <HistoryItem questionTitle={'둘 중 하나에 취직한다면?'} />
+      <HistoryItem questionTitle={'둘 중 하나에 취직한다면?'} />
+    </div>
+  );
+};
+
+export default HistoryList;
