eca15700af326b00fa07bd18e7c9833ec7a99d0d
Suyeon <msjgr2801@gmail.com>
2023-10-31 11:11:47 +0900

    ✨ Feat: Question 페이지 구성

--- src/pages/Question/Question.tsx
+++ src/pages/Question/Question.tsx
@@ -1,5 +1,18 @@
+import styled from '@emotion/styled';
+
+const QuestionContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
 const Question = () => {
-  return <></>;
+  return (
+    <QuestionContainer>
+      <QuestionHeader />
+      <QuestionForm />
+      <QuestionChat />
+    </QuestionContainer>
+  );
 };
 
 export default Question;
