31edf740555d6240f2f9b0a0052fb9bad6eecb0c
Suyeon <msjgr2801@gmail.com>
2023-10-31 12:24:10 +0900

    🔨 Refactoring: CategoryButton -> CircleButton 사용

--- src/components/common/CircleButton/CircleButton.tsx
+++ src/components/common/CircleButton/CircleButton.tsx
@@ -10,7 +10,7 @@ const CircleButton = ({ label, icon, active }: CircleButtonProps) => {
   const IconComponent = icon;
   const activeButtonStyle = active
     ? 'bg-base-primary'
-    : 'border border-grey-300 bg-base-white';
+    : 'border border-grey-200 bg-base-white';
   const activeIconStyle = active
     ? 'stroke-base-white fill-base-white'
     : 'stroke-grey-300 fill-grey-300';
@@ -21,10 +21,10 @@ const CircleButton = ({ label, icon, active }: CircleButtonProps) => {
         className={`btn-circle btn-sm relative overflow-hidden ${activeButtonStyle} group hover:border-0 hover:bg-base-primary`}
       >
         <IconComponent
-          className={`absolute left-[50%] top-[50%] z-20 w-[50%]  translate-x-[-50%] translate-y-[-50%] ${activeIconStyle} group-hover:stroke-base-white`}
+          className={`absolute left-[50%] top-[50%] z-20 w-[50%]  translate-x-[-50%] translate-y-[-50%] ${activeIconStyle} group-hover:fill-base-white group-hover:stroke-base-white`}
         />
       </button>
-      {label && <div className="text-grey-500">{label}</div>}
+      {label && <div className="font-small py-1 text-grey-500">{label}</div>}
     </div>
   );
 };
