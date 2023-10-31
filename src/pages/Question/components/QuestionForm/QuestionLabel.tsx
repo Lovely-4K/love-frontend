80ff7feac9bece6a6f8751e21caac776c9f4b465
Suyeon <msjgr2801@gmail.com>
2023-10-31 14:24:55 +0900

    ✨ Feat: QuestionLabel 구현

--- src/pages/Question/components/QuestionForm/QuestionLabel.tsx
+++ src/pages/Question/components/QuestionForm/QuestionLabel.tsx
@@ -1,5 +1,12 @@
 const QuestionLabel = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="flex items-center justify-center rounded-xl bg-base-secondary px-3 py-5 text-base-white">
+        세미콜론 쓰기 vs 안 쓰기
+      </div>
+      <div>나의 답변은 무엇인가요?</div>
+    </div>
+  );
 };
 
 export default QuestionLabel;
