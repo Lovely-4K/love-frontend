f6d8212022b67aa81a34ab363da57797c270b68e
Suyeon <msjgr2801@gmail.com>
2023-10-31 15:37:41 +0900

    ✨ Feat: 새 질문 생성 UI 적용

--- src/pages/Question/components/QuestionForm/QuestionForm.tsx
+++ src/pages/Question/components/QuestionForm/QuestionForm.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import QuestionFormCreate from './QuestionFormCreate';
 import QuestionFormLabel from './QuestionFormLabel';
 import QuestionFormSelect from './QuestionFormSelect';
 
@@ -6,15 +7,21 @@ const QuestionFormContainer = styled.div``;
 
 /** @todo - selected Item 이 없으면 button disabled 처리하기 */
 const QuestionForm = () => {
+  const myAnswerStatus: boolean = true;
+  const myLoverStatus: boolean = true;
+
   return (
     <QuestionFormContainer>
       <QuestionFormLabel />
       <QuestionFormSelect />
-      <div>
-        <button className="btn-small btn-primary float-right w-full rounded-xl hover:border-none hover:bg-base-secondary">
-          결정
-        </button>
-      </div>
+      {!myAnswerStatus && (
+        <div className="flex w-full justify-end">
+          <button className="btn-small btn-primary w-full rounded-xl hover:border-none hover:bg-base-secondary">
+            결정
+          </button>
+        </div>
+      )}
+      {myAnswerStatus && myLoverStatus && <QuestionFormCreate />}
     </QuestionFormContainer>
   );
 };
