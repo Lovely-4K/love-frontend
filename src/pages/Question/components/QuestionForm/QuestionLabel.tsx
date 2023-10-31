bb94ef2bb61c6c08dcc6dedb3a1812efab80365a
Suyeon <msjgr2801@gmail.com>
2023-10-31 14:42:18 +0900

    💄 Style: QuestionLabel 마진 추가

--- src/pages/Question/components/QuestionForm/QuestionLabel.tsx
+++ src/pages/Question/components/QuestionForm/QuestionLabel.tsx
@@ -1,6 +1,6 @@
 const QuestionLabel = () => {
   return (
-    <div className="flex flex-col gap-5">
+    <div className="mt-3 flex flex-col gap-5">
       <div className="flex items-center justify-center rounded-xl bg-base-secondary px-3 py-5 text-base-white">
         세미콜론 쓰기 vs 안 쓰기
       </div>
