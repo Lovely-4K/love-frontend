17f5bf76d0d75b97d963d86c303411c9b9c57683
Suyeon <msjgr2801@gmail.com>
2023-10-31 12:03:48 +0900

    ✨ Feat: QuestionHeader UI 구현
    - font 크기 사이즈 추가
    - menu svg stroke 속성 수정

--- src/pages/Question/Question.tsx
+++ src/pages/Question/Question.tsx
@@ -4,6 +4,13 @@ import { QuestionHeader, QuestionForm, QuestionChat } from './components';
 const QuestionContainer = styled.div`
   width: 100%;
   height: 100%;
+  margin: 0 auto;
+  padding: 3rem 4rem;
+
+  @media screen and (min-width: 1024px) {
+    width: 70%;
+    padding: 4rem 10rem;
+  }
 `;
 
 const Question = () => {
--- src/pages/Question/components/QuestionHeader/QuestionHeader.tsx
+++ src/pages/Question/components/QuestionHeader/QuestionHeader.tsx
@@ -1,5 +1,15 @@
+import { IconMenu } from '~/assets/icons';
+import { CircleButton } from '~/components/common';
+
 const QuestionHeader = () => {
-  return <div></div>;
+  return (
+    <div className="flex justify-between">
+      <div className="lg:font-title-large font-title">오늘의 질문</div>
+      <div>
+        <CircleButton label="전체" icon={IconMenu} active={false} />
+      </div>
+    </div>
+  );
 };
 
 export default QuestionHeader;
