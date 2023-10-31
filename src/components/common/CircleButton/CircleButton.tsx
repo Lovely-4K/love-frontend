ee1fc074bc09f8390ce0db922863dcbdfcacabc7
Suyeon <msjgr2801@gmail.com>
2023-10-31 14:40:59 +0900

    💄 Style: CircleButton hover 시 색상 변경

--- src/components/common/CircleButton/CircleButton.tsx
+++ src/components/common/CircleButton/CircleButton.tsx
@@ -18,7 +18,7 @@ const CircleButton = ({ label, icon, active }: CircleButtonProps) => {
   return (
     <div className="flex flex-col items-center">
       <button
-        className={`btn-circle btn-sm relative overflow-hidden ${activeButtonStyle} group hover:border-0 hover:bg-base-primary`}
+        className={`btn-circle btn-sm relative overflow-hidden ${activeButtonStyle} group hover:border-0 hover:bg-base-secondary`}
       >
         <IconComponent
           className={`absolute left-[50%] top-[50%] z-20 w-[50%]  translate-x-[-50%] translate-y-[-50%] ${activeIconStyle} group-hover:fill-base-white group-hover:stroke-base-white`}
