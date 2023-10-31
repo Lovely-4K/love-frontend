64674f01e699d0c8afa5a9e49d5f3a70f9c26f1c
Suyeon <msjgr2801@gmail.com>
2023-10-31 16:49:49 +0900

    📝 Chore: QuestionList -> QuestionHistory 로 이름 변경

---
+++ src/pages/QuestionList/QuestionHistory.tsx
@@ -0,0 +1,9 @@
+import { QuestionContainer } from '~/pages/Question/Question';
+
+const QuestionHistory = () => {
+  return (
+    <QuestionContainer className="flex flex-col gap-3"></QuestionContainer>
+  );
+};
+
+export default QuestionHistory;
