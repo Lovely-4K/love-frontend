a6cd77f2ba87ade1b2892f5967f5096a0a041640
HongGunWoo <foxrain.gg@gmail.com>
2023-11-02 15:53:38 +0900

    📝 Chore: conflict 해결

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,23 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Main, Question, QuestionHistory, QuestionCreate } from '~/pages';
+import { createBrowserRouter } from 'react-router-dom';
+import { Calendar, Login, Main } from '~/pages';
 import Layout from './Layout';
+import PATHS from './paths';
 
 /** @todo 인증 사용자가 아니면 PrivateRoute 로 이동, 인증 사용자일 시 PublicRouter 로 이동 */
 
-const Router = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Main />} />
-          <Route path="/question" element={<Question />} />
-          <Route path="/question/history" element={<QuestionHistory />} />
-          <Route path="/question/create" element={<QuestionCreate />} />
-          <Route path="/diary" element={<Main />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-};
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: PATHS.MAIN,
+        element: <Main />,
+      },
 
-export default Router;
+      {
+        path: PATHS.CALENDAR,
+        element: <Calendar />,
+      },
+    ],
+  },
+  {
+    path: PATHS.LOGIN,
+    element: <Login />,
+  },
+]);
+
+export default router;
