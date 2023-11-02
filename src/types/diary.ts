7872c46945b2ac59066cc41d08cd383129af54b4
cloud0406 <cloud0406@naver.com>
2023-11-02 17:19:51 +0900

    ✨ Feat: couple, diary 응답 값 type 추가

---
+++ src/types/diary.ts
@@ -0,0 +1,50 @@
+interface Diary {
+  kakaoMapId: number;
+  datingDay: number[];
+  score: number;
+  category: string;
+  boyText: string;
+  girlText: string;
+  pictures: {
+    firstImage: string | null;
+    secondImage: string | null;
+    thirdImage: string | null;
+    fourthImage: string | null;
+    fifthImage: string | null;
+  };
+}
+
+interface Diarys {
+  content: {
+    diaryId: number;
+    kakaoMapId: number;
+    imageUrl: string;
+  }[];
+  pageable: {
+    pageNumber: number;
+    pageSize: number;
+    sort: {
+      empty: boolean;
+      sorted: boolean;
+      unsorted: boolean;
+    };
+    offset: number;
+    paged: boolean;
+    unpaged: boolean;
+  };
+  totalPages: number;
+  totalElements: number;
+  last: boolean;
+  size: number;
+  number: number;
+  sort: {
+    empty: boolean;
+    sorted: boolean;
+    unsorted: boolean;
+  };
+  numberOfElements: number;
+  first: boolean;
+  empty: boolean;
+}
+
+export type { Diary, Diarys };
