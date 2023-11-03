bd3aeaa87f4ba08c7f98ee0dd501b0d1cb2424dd
Suyeon <msjgr2801@gmail.com>
2023-11-03 14:51:01 +0900

    🔨 Refactoring: 라우팅 경로 상수화 변수 사용

--- src/pages/QuestionHistory/components/HistoryHeader.tsx
+++ src/pages/QuestionHistory/components/HistoryHeader.tsx
@@ -1,9 +1,13 @@
+import { paths } from '~/router';
+
 import { NavigationHeader } from '~/components/domain';
 
 const HistoryHeader = () => {
+  const { QUESTION } = paths;
+
   return (
     <div className="mx-[-3rem]">
-      <NavigationHeader pageTitle="우리의 질문들" prevPageLink="/question" />
+      <NavigationHeader pageTitle="우리의 질문들" prevPageLink={QUESTION} />
     </div>
   );
 };
