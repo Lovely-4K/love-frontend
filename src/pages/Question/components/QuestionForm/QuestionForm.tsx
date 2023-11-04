b5ea2c8131d9e3ec7fb28c0cdc346b19e8489c83
Suyeon <msjgr2801@gmail.com>
2023-11-04 19:17:20 +0900

    🔨 Refactoring: 로직 & UI 분리하기

--- src/pages/Question/components/QuestionForm/QuestionForm.tsx
+++ src/pages/Question/components/QuestionForm/QuestionForm.tsx
@@ -1,46 +1,30 @@
-import styled from '@emotion/styled';
-
+import useGetQuestion from '../../hooks/useGetQuestion';
 import QuestionFormCreate from './QuestionFormCreate';
 import QuestionFormLabel from './QuestionFormLabel';
 import QuestionFormSelect from './QuestionFormSelect';
 import { QuestionProvider } from '~/pages/Question/contexts/QuestionContext';
-import { usePatchAnswerQuestion } from '~/pages/Question/hooks/usePatchAnswerQuestion';
-
-const QuestionFormContainer = styled.div``;
 
 /** @todo - selected Item 이 없으면 button disabled 처리하기 */
 const QuestionForm = () => {
   const myAnswerStatus: boolean = false;
   const myLoverStatus: boolean = true;
 
-  const patchAnswerMutation = usePatchAnswerQuestion();
-
-  const handlePatchAnswer = async (choiceNumber: number) => {
-    try {
-      const result = await patchAnswerMutation.mutateAsync({ choiceNumber });
-      console.log(result);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  useGetQuestion();
 
   return (
     <QuestionProvider>
-      <QuestionFormContainer>
+      <div>
         <QuestionFormLabel />
         <QuestionFormSelect />
         {!myAnswerStatus && (
           <div className="flex w-full justify-end">
-            <button
-              onClick={() => handlePatchAnswer(1)}
-              className="btn-small btn-primary w-full rounded-xl hover:border-none hover:bg-base-secondary"
-            >
+            <button className="btn-small btn-primary w-full rounded-xl hover:border-none hover:bg-base-secondary">
               결정
             </button>
           </div>
         )}
         {myAnswerStatus && myLoverStatus && <QuestionFormCreate />}
-      </QuestionFormContainer>
+      </div>
     </QuestionProvider>
   );
 };
