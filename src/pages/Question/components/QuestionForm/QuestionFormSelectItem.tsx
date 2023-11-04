23bb1a352a17d66a63ccd5ab9af1bcd720a21d99
Suyeon <msjgr2801@gmail.com>
2023-11-04 22:41:39 +0900

    💄 Style: SelectedItem 공통 패딩 속성 주기

--- src/pages/Question/components/QuestionForm/QuestionFormSelectItem.tsx
+++ src/pages/Question/components/QuestionForm/QuestionFormSelectItem.tsx
@@ -13,12 +13,12 @@ const QuestionFormSelectItem = ({
   const activeStyle =
     itemIndex === userAnswer
       ? 'border-none bg-primary text-base-white hover:bg-base-secondary'
-      : 'border border-grey-200 py-3 hover:bg-grey-100';
+      : 'border border-grey-200  hover:bg-grey-100';
 
   return (
     <div
       onClick={() => setUserAnswer(itemIndex)}
-      className={`flex flex-grow cursor-pointer items-center justify-center rounded-xl ${activeStyle}`}
+      className={`flex flex-grow cursor-pointer items-center justify-center rounded-xl py-3 ${activeStyle}`}
     >
       {answer}
     </div>
