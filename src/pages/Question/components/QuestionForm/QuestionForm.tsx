e32592dbff7d4d434b37abd1c3c299a729eebfdd
Suyeon <msjgr2801@gmail.com>
2023-11-04 22:49:28 +0900

    ✨ Feat: 결정 버튼 클릭 시 답변 API 연결
    - QuestionForm 와 버튼 분리
    - 버튼 disabled 조건 추가

--- src/pages/Question/components/QuestionForm/QuestionForm.tsx
+++ src/pages/Question/components/QuestionForm/QuestionForm.tsx
@@ -4,11 +4,7 @@ import QuestionFormLabel from './QuestionFormLabel';
 import QuestionFormSelect from './QuestionFormSelect';
 import { QuestionProvider } from '~/pages/Question/contexts/QuestionContext';
 
-/** @todo - selected Item 이 없으면 button disabled 처리하기 */
 const QuestionForm = () => {
-  const myAnswerStatus: boolean = false;
-  const myLoverStatus: boolean = true;
-
   useGetQuestion();
 
   return (
@@ -16,14 +12,7 @@ const QuestionForm = () => {
       <div>
         <QuestionFormLabel />
         <QuestionFormSelect />
-        {!myAnswerStatus && (
-          <div className="flex w-full justify-end">
-            <button className="btn-small btn-primary w-full rounded-xl hover:border-none hover:bg-base-secondary">
-              결정
-            </button>
-          </div>
-        )}
-        {myAnswerStatus && myLoverStatus && <QuestionFormCreate />}
+        <QuestionFormCreate />
       </div>
     </QuestionProvider>
   );
--- src/pages/Question/components/QuestionForm/QuestionFormSelect.tsx
+++ src/pages/Question/components/QuestionForm/QuestionFormSelect.tsx
@@ -2,18 +2,34 @@ import QuestionFormSelectItem from './QuestionFormSelectItem';
 import useQuestionForm from '~/pages/Question/hooks/useQuestionForm';
 
 const QuestionFormSelect = () => {
-  const { answers } = useQuestionForm();
+  const { userAnswer, answers, handleSubmitUserAnswer } = useQuestionForm();
+  console.log(userAnswer);
 
   return (
-    <div className="my-3 flex flex-col gap-3 lg:flex-row">
-      {answers.map((answer, index) => {
-        return (
-          answer && (
-            <QuestionFormSelectItem itemIndex={index + 1} answer={answer} />
-          )
-        );
-      })}
-    </div>
+    <>
+      <div className="my-3 flex flex-col gap-3 lg:flex-row">
+        {answers.map((answer, index) => {
+          return (
+            answer && (
+              <QuestionFormSelectItem
+                key={index}
+                itemIndex={index + 1}
+                answer={answer}
+              />
+            )
+          );
+        })}
+      </div>
+      <div className="flex w-full justify-end">
+        <button
+          disabled={userAnswer === -1}
+          onClick={handleSubmitUserAnswer}
+          className="btn-small btn-primary w-full rounded-xl hover:border-none hover:bg-base-secondary disabled:cursor-not-allowed disabled:bg-grey-300"
+        >
+          결정
+        </button>
+      </div>
+    </>
   );
 };
 
