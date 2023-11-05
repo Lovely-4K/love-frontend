4d9f4605d041ebfcf50d844d3e5ea2a2d87b2db0
cloud0406 <cloud0406@naver.com>
2023-11-05 12:48:31 +0900

    ✨ Feat: Question History 질문 목록 불러오기 API 연동

--- src/pages/QuestionHistory/components/HistoryList/HistoryList.tsx
+++ src/pages/QuestionHistory/components/HistoryList/HistoryList.tsx
@@ -1,24 +1,12 @@
 import HistoryItem from './HistoryItem';
-import { useGetQuestions } from '~/pages/QuestionHistory/hooks/useGetQuestions';
+import useGetQuestions from '~/pages/QuestionHistory/hooks/useGetQuestions';
 
 const HistoryList = () => {
   const { data: questions } = useGetQuestions();
 
-  const defaultQuestions = {
-    answeredQuestions: [
-      { questionId: 1, questionContent: 'Default Question 1' },
-      { questionId: 2, questionContent: 'Default Question 2' },
-      { questionId: 3, questionContent: 'Default Question 2' },
-      { questionId: 4, questionContent: 'Default Question 2' },
-    ],
-  };
-
-  const answeredQuestions =
-    questions?.answeredQuestions || defaultQuestions.answeredQuestions;
-
   return (
     <div className="flex flex-col gap-3">
-      {answeredQuestions.map(({ questionId, questionContent }) => (
+      {questions?.answeredQuestions.map(({ questionId, questionContent }) => (
         <HistoryItem questionTitle={questionContent} key={questionId} />
       ))}
     </div>
