5c79810be30b987a5d82b3a385f65998c731d32d
Suyeon <msjgr2801@gmail.com>
2023-11-07 14:32:59 +0900

    🚀 Build: 스토리북 삭제

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:storybook/recommended',
     'plugin:prettier/recommended',
     'plugin:@tanstack/eslint-plugin-query/recommended',
     '@rushstack/eslint-config/profile/web-app',
