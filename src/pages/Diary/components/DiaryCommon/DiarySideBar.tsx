66118ee8fcbd94872a1d7cdcba9d6ced7cf786c3
cloud0406 <cloud0406@naver.com>
2023-11-08 01:16:01 +0900

    💄 Style: 사이드바 arrow open, close시 rotate 변경

--- src/pages/Diary/components/DiaryCommon/DiarySideBar.tsx
+++ src/pages/Diary/components/DiaryCommon/DiarySideBar.tsx
@@ -13,6 +13,8 @@ const DiarySideBar = () => {
     ? 'translate-y-0 lg:translate-x-0'
     : 'translate-y-full h-0 lg:-translate-x-[98%] lg:translate-y-0';
 
+  const arrowStyle = isOpen ? 'rotate-180 lg:-rotate-90' : 'lg:rotate-90';
+
   return (
     <div
       className={`absolute bottom-24 z-30 flex h-[23rem] w-full justify-center rounded-t-xl bg-base-white p-7 shadow-[0rem_-0.15rem_0.15rem_0rem_rgba(0,0,0,0.25)] transition-all duration-300 lg:bottom-0 lg:left-28 lg:h-screen lg:w-[23rem] lg:rounded-r-xl lg:shadow-[0.15rem_0.15rem_0.15rem_0rem_rgba(0,0,0,0.25)] ${openStyle}`}
@@ -21,11 +23,7 @@ const DiarySideBar = () => {
         className="absolute bottom-full flex h-7 w-10 items-center justify-center rounded-t-xl border-2 border-b-0 border-grey-300 bg-base-white text-xl text-base-black lg:left-full lg:top-1/2 lg:h-10 lg:w-7 lg:rounded-l-none lg:rounded-r-xl lg:border-b-2 lg:border-l-0"
         onClick={toggleSideBar}
       >
-        <IconTopArrow
-          className={`h-4 w-4 fill-grey-400 lg:rotate-90 ${
-            isOpen && 'rotate-180 lg:-rotate-90'
-          }`}
-        />
+        <IconTopArrow className={`h-4 w-4 fill-grey-400 ${arrowStyle}`} />
       </button>
       <DiaryContent />
     </div>
