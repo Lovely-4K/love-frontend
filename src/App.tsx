424a90cd489ea4afe57f1c1335b9c11ab4020afb
Suyeon <msjgr2801@gmail.com>
2023-11-07 14:43:24 +0900

    🚀 Build: react-query-devtools 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '~/router';
 
@@ -9,6 +10,7 @@ const App = () => {
     <>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </>
   );
