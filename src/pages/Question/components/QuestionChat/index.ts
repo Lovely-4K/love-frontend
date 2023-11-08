f0a26bbad7722bd7d7aea9d990b469f637d1108a
Suyeon <msjgr2801@gmail.com>
2023-11-08 14:30:36 +0900

    📝 Chore: Question 관련 페이지들 index.ts 파일 삭제

--- src/pages/Question/components/QuestionChat/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as QuestionChat } from './QuestionChat';
--- src/pages/Question/components/index.ts
+++ src/pages/Question/components/index.ts
@@ -1,3 +1,3 @@
-export * from './QuestionHeader';
-export * from './QuestionForm';
-export * from './QuestionChat';
+export { default as QuestionHeader } from './QuestionHeader/QuestionHeader.tsx';
+export { default as QuestionForm } from './QuestionForm/QuestionForm.tsx';
+export { default as QuestionChat } from './QuestionChat/QuestionChat.tsx';
