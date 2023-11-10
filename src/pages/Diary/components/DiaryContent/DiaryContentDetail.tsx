620e8c96b31b4263ff7d4c3383902a5c4cb5d5fa
cloud0406 <cloud0406@naver.com>
2023-11-10 17:26:42 +0900

    ✨ Feat: DiaryContent 이미지 provider 적용

--- src/pages/Diary/components/DiaryContent/DiaryContentDetail.tsx
+++ src/pages/Diary/components/DiaryContent/DiaryContentDetail.tsx
@@ -1,5 +1,6 @@
 import DiaryContentImgs from '~/pages/Diary/components/DiaryContent/DiaryContentImgs';
 import DiaryContentText from '~/pages/Diary/components/DiaryContent/DiaryContentText';
+import { DiaryImgsProvider } from '~/pages/Diary/contexts/DiaryImgsContext';
 
 const DiaryContentDetail = () => {
   return (
@@ -8,7 +9,9 @@ const DiaryContentDetail = () => {
         다이어리 내용
       </span>
       <div className="rounded-xl border border-grey-200 px-3 py-5">
-        <DiaryContentImgs />
+        <DiaryImgsProvider>
+          <DiaryContentImgs />
+        </DiaryImgsProvider>
         <DiaryContentText />
       </div>
     </div>
--- src/pages/Diary/components/DiaryContent/DiaryContentText.tsx
+++ src/pages/Diary/components/DiaryContent/DiaryContentText.tsx
@@ -5,18 +5,14 @@ const DiaryContentText = () => {
   const { editMode } = useContext(DiaryContentContext);
 
   return (
-    <>
-      <div className="flex flex-col items-center justify-center gap-4 rounded-xl">
-        <textarea
-          className={`font-small textarea h-[15rem] w-[95%] resize-none placeholder:font-small placeholder:text-grey-300 ${
-            editMode || 'focus:outline-none'
-          }`}
-          maxLength={200}
-          placeholder="데이트를 하며 좋았던 순간을 기록해보세요"
-          readOnly={editMode ? false : true}
-        ></textarea>
-      </div>
-    </>
+    <div className="flex flex-col items-center justify-center gap-4 rounded-xl">
+      <textarea
+        className="font-small textarea h-[15rem] w-[95%] resize-none placeholder:font-small placeholder:text-grey-300 focus:outline-none"
+        maxLength={200}
+        placeholder="데이트를 하며 좋았던 순간을 기록해보세요"
+        readOnly={editMode ? false : true}
+      ></textarea>
+    </div>
   );
 };
 
