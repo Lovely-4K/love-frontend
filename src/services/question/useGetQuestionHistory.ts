6f1eb8b06c330be7de3fe4cf7e5fead242d59f10
Suyeon <msjgr2801@gmail.com>
2023-11-13 18:11:03 +0900

    📝 Chore: 폴더명 변경

---
+++ src/services/question/useGetQuestionHistory.ts
@@ -0,0 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
+import { QuestionHistoryPreview } from '~/types';
+import apiClient from '~/api/apiClient';
+
+const getQuestionHistory = async (
+  coupleId: number = 1,
+  lastQuestionId: number = 0,
+): Promise<QuestionHistoryPreview[]> => {
+  const url = `/questions?id=${lastQuestionId}&coupleId=${coupleId}&limit=10`;
+  const response = await apiClient.get(url);
+
+  return response.data;
+};
+
+const useGetQuestionHistory = (
+  coupleId: number = 1,
+  lastQuestionId: number = 0,
+) => {
+  return useQuery({
+    queryKey: ['questions', coupleId, lastQuestionId],
+    queryFn: () => getQuestionHistory(coupleId, lastQuestionId),
+  });
+};
+
+export default useGetQuestionHistory;
