3d0613b543e44ff276f42ac5ea45b2e68d40653f
Suyeon <msjgr2801@gmail.com>
2023-11-13 12:16:10 +0900

    🚨 Error: 답변 삭제 버튼 에러 해결
    - 삭제버튼 클릭 시 dataset 이 제대로 받아와지지 않던 문제 수정

--- src/pages/QuestionCreate/components/QuestionCreateForm/FormAnswer.tsx
+++ src/pages/QuestionCreate/components/QuestionCreateForm/FormAnswer.tsx
@@ -4,13 +4,13 @@ import FormAnswerInput from './FormAnswerInput';
 
 /** @todo-key 값 추후 바꿀 방법 생각해보기 */
 const FormAnswer = () => {
-  const { answers, handleDeleteButton } = useQuestionCreateForm();
+  const { answers } = useQuestionCreateForm();
 
   return (
     <div>
       <div className="flex flex-col gap-3">
         <FormAnswerInput />
-        <div onClick={handleDeleteButton} className="flex flex-col gap-3">
+        <div className="flex flex-col gap-3">
           {answers.map((answer, index) => (
             <FormAnswerCard key={index} index={index} answer={answer} />
           ))}
