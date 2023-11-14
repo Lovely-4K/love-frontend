92ba4d74679d6b785579d0a4f3af380438fc21f3
Suyeon <msjgr2801@gmail.com>
2023-11-14 14:35:37 +0900

    📝 Chore: 서버 도메인 주소 변경

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const apiClient = axios.create({ baseURL: 'http://43.201.122.97/v1' });
+const apiClient = axios.create({ baseURL: 'https://love-back.kro.kr/v1' });
 
 export default apiClient;
