7ae1da50824a4c55da20942e5a73538c6267c4f2
Suyeon <msjgr2801@gmail.com>
2023-11-15 16:45:43 +0900

    ✨ Feat: vite proxy 에 env 변수 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: [{ find: '~', replacement: path.resolve(__dirname, 'src') }],
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://love-back.kro.kr/v1',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false,
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react(), svgr()],
+    resolve: {
+      alias: [{ find: '~', replacement: path.resolve(__dirname, 'src') }],
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_API_END_POINT,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+          secure: false,
+        },
       },
     },
-  },
-});
+  });
+};
