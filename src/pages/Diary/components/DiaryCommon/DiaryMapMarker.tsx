e0f5ab7af4d24f61910308c67f6bae0114629e28
cloud0406 <cloud0406@naver.com>
2023-11-16 18:44:04 +0900

    ✨ Feat: 마커 커스텀 및 svg 파일 추가

--- src/pages/Diary/components/DiaryCommon/DiaryMapMarker.tsx
+++ src/pages/Diary/components/DiaryCommon/DiaryMapMarker.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { CustomOverlayMap, MapMarker } from 'react-kakao-maps-sdk';
+import { MapMarkerGone } from '~/assets/icons';
 import DiaryMapInfo from '~/pages/Diary/components/DiaryCommon/DiaryMapInfo';
 import useCurrentLocation from '~/pages/Diary/hooks/useCurrentLoaction';
 import useInputRef from '~/pages/Diary/hooks/useInputRef';
@@ -18,14 +19,32 @@ const DiaryMapMarker = ({ userPosition }: DiaryMapMarkerProps) => {
     setInfo(marker);
   };
 
+  if (!userPosition) return;
+
   return (
     <>
-      <MapMarker position={userPosition} />
+      <MapMarker
+        position={userPosition}
+        image={{
+          src: 'src/assets/icons/currentMarker.svg',
+          size: {
+            width: 40,
+            height: 65,
+          }, // 마커이미지의 크기입니다
+        }}
+      />
       {markers.map((marker) => (
         <MapMarker
           key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
           position={marker.position}
           onClick={() => handleMarker(marker)}
+          image={{
+            src: 'src/assets/icons/mapMarkerGone.svg',
+            size: {
+              width: 35,
+              height: 40,
+            }, // 마커이미지의 크기입니다
+          }}
         />
       ))}
     </>
