814db97b64bc9635d7b62122b3cad182727853b9
cloud0406 <cloud0406@naver.com>
2023-11-19 22:02:03 +0900

    📝 Chore: sideBar context 제거 (diary context로 이동)

--- src/pages/Diary/Diary.tsx
+++ src/pages/Diary/Diary.tsx
@@ -2,15 +2,12 @@ import { DiarySideBar } from '~/pages/Diary/components/DiaryCommon';
 import DiaryMap from '~/pages/Diary/components/DiaryCommon/DiaryMap';
 import { DiaryMapProvider } from '~/pages/Diary/contexts/DiaryMapContext';
 import { DiaryProvider } from '~/pages/Diary/contexts/DiaryProvider';
-import { DiarySideBarProvider } from '~/pages/Diary/contexts/DiarySideBarContext';
 
 const Diary = () => {
   return (
     <DiaryProvider>
       <div className="h-full w-full">
-        <DiarySideBarProvider>
-          <DiarySideBar />
-        </DiarySideBarProvider>
+        <DiarySideBar />
         <DiaryMapProvider>
           <DiaryMap />
         </DiaryMapProvider>
