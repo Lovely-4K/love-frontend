3e0225ccbc0f2262383fc40d476aa1077a02d62d
cloud0406 <cloud0406@naver.com>
2023-11-20 01:22:40 +0900

    📝 Chore: tailwind 린트 에러 커밋

--- src/pages/Diary/components/DiaryContent/DiaryContentText.tsx
+++ src/pages/Diary/components/DiaryContent/DiaryContentText.tsx
@@ -7,7 +7,7 @@ const DiaryContentText = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-4 rounded-xl">
       <textarea
-        className="font-small textarea h-[15rem] w-[95%] resize-none placeholder:font-small placeholder:text-grey-300 focus:outline-none"
+        className="font-small placeholder:font-small textarea h-[15rem] w-[95%] resize-none placeholder:text-grey-300 focus:outline-none"
         maxLength={200}
         placeholder="데이트를 하며 좋았던 순간을 기록해보세요"
         readOnly={editMode ? false : true}
