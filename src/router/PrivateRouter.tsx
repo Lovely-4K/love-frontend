83cd68153f56d7bfd5122d490d190dda0b36cb84
Suyeon <msjgr2801@gmail.com>
2023-11-20 15:08:53 +0900

    🔨 Refactoring: 훅이 아닌 함수 util 로 분리

--- src/router/PrivateRouter.tsx
+++ src/router/PrivateRouter.tsx
@@ -1,10 +1,10 @@
 import { Navigate, Outlet } from 'react-router-dom';
-import useCheckTokenParams from '~/hooks/useCheckTokenParams';
 import useLogin from '~/hooks/useLogin';
+import checkTokenParams from '~/utils/checkTokenParams';
 
 /** @todo: 유저가 localStorage 토큰 값을 수정하는 경우 checkAuth 등으로 에러 표시 필요 */
 const PrivateRouter = () => {
-  const token = useCheckTokenParams('token');
+  const token = checkTokenParams('token');
   const { isLoggedIn } = useLogin(token);
 
   return isLoggedIn() ? <Outlet /> : <Navigate to={`login`} replace />;
--- src/utiils/checkTokenParams.ts
+++ src/utiils/checkTokenParams.ts
@@ -1,8 +1,8 @@
-const useCheckTokenParams = (key: string) => {
+const checkTokenParams = (key: string) => {
   const urlParams = new URL(location.href).searchParams;
   const value = urlParams.get(key);
 
   return value;
 };
 
-export default useCheckTokenParams;
+export default checkTokenParams;
