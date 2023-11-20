f3e4869f10f9f1da0b972a3a6de4e455754d9623
Suyeon <msjgr2801@gmail.com>
2023-11-20 15:19:29 +0900

    🔨 Refactoring: checkTokenParam 확장성 수정

--- src/router/PrivateRouter.tsx
+++ src/router/PrivateRouter.tsx
@@ -1,10 +1,10 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import useLogin from '~/hooks/useLogin';
-import checkTokenParams from '~/utils/checkTokenParams';
+import checkParams from '~/utils/checkParams';
 
 /** @todo: 유저가 localStorage 토큰 값을 수정하는 경우 checkAuth 등으로 에러 표시 필요 */
 const PrivateRouter = () => {
-  const token = checkTokenParams('token');
+  const token = checkParams('token');
   const { isLoggedIn } = useLogin(token);
 
   return isLoggedIn() ? <Outlet /> : <Navigate to={`login`} replace />;
--- src/utils/checkParams.ts
+++ src/utils/checkParams.ts
@@ -1,8 +1,8 @@
-const checkTokenParams = (key: string) => {
+const checkParams = (key: string) => {
   const urlParams = new URL(location.href).searchParams;
   const value = urlParams.get(key);
 
   return value;
 };
 
-export default checkTokenParams;
+export default checkParams;
