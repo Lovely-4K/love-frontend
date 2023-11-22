42f10ed8d988ba15d51f23892fd41058d7f08133
Suyeon <msjgr2801@gmail.com>
2023-11-22 18:12:20 +0900

    ✨ Feat: DiaryContent 에 조회 API 연결하기

--- src/pages/Diary/components/DiaryCommon/DiaryHeader.tsx
+++ src/pages/Diary/components/DiaryCommon/DiaryHeader.tsx
@@ -1,20 +1,12 @@
-import { Link } from 'react-router-dom';
 import { IconTopArrow } from '~/assets/icons';
 
-interface DiaryHeaderProps {
-  keyword: string;
-  prevPageLink: string;
-}
-
-const DiaryHeader = ({ keyword, prevPageLink }: DiaryHeaderProps) => {
+const DiaryHeader = () => {
   return (
     <div className="font-title flex gap-2 font-bold">
-      <Link to={prevPageLink}>
-        <button className="text-grey-300">
-          <IconTopArrow className="h-4 w-4 -rotate-90 fill-grey-300" />
-        </button>
-      </Link>
-      <span>{keyword}</span>
+      <button className="text-grey-300">
+        <IconTopArrow className="h-4 w-4 -rotate-90 fill-grey-300" />
+      </button>
+      <span>김선익 내과</span>
     </div>
   );
 };
