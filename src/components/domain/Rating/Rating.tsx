e5364f39f3a26de70e2d4ba1c169da600bfe8fae
Suyeon <msjgr2801@gmail.com>
2023-11-22 17:37:23 +0900

    ✨ Feat: Rating 컴포넌트에 default 값 추가

--- src/components/domain/Rating/Rating.tsx
+++ src/components/domain/Rating/Rating.tsx
@@ -1,40 +1,41 @@
+import { useState } from 'react';
+
 interface RatingProps {
   readonly: boolean;
+  defaultScore?: undefined | number;
 }
 
-const Rating = ({ readonly }: RatingProps) => {
+interface RatingInputProps {
+  assignedScore: number;
+  activateStatus: boolean;
+}
+const Rating = ({ readonly, defaultScore = 4 }: RatingProps) => {
+  const [score, setScore] = useState(defaultScore);
+  const ratingInputArray = [...new Array(5)].map((_, index) => index < score);
+  const RatingInput = ({ assignedScore, activateStatus }: RatingInputProps) => {
+    return (
+      <input
+        onClick={() => setScore(assignedScore)}
+        type="radio"
+        name="rating-3"
+        className="mask mask-heart w-5 bg-base-primary"
+        disabled={readonly}
+        checked={activateStatus}
+      />
+    );
+  };
+
   return (
     <div className="rating gap-1">
-      <input
-        type="radio"
-        name="rating-3"
-        className="mask mask-heart w-5 bg-base-primary"
-        disabled={readonly}
-      />
-      <input
-        type="radio"
-        name="rating-3"
-        className="mask mask-heart w-5 bg-base-primary"
-        disabled={readonly}
-      />
-      <input
-        type="radio"
-        name="rating-3"
-        className="mask mask-heart w-5 bg-base-primary"
-        disabled={readonly}
-      />
-      <input
-        type="radio"
-        name="rating-3"
-        className="mask mask-heart w-5 bg-base-primary"
-        disabled={readonly}
-      />
-      <input
-        type="radio"
-        name="rating-3"
-        className="mask mask-heart w-5 bg-base-primary"
-        disabled={readonly}
-      />
+      {ratingInputArray.map((activateStatus, index) => {
+        return (
+          <RatingInput
+            key={index}
+            assignedScore={index + 1}
+            activateStatus={activateStatus}
+          />
+        );
+      })}
     </div>
   );
 };
