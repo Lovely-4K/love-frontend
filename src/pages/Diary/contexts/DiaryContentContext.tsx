1345548d8e563d4729f1211ea7074ac586195334
Suyeon <msjgr2801@gmail.com>
2023-11-23 16:34:48 +0900

    ✨ Feat: form 에 사용되는 각 값 state 로 변경

--- src/pages/Diary/contexts/DiaryContentContext.tsx
+++ src/pages/Diary/contexts/DiaryContentContext.tsx
@@ -1,7 +1,27 @@
 import { PropsWithChildren, createContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Diary } from '~/types';
+import { Diary, Pictures } from '~/types';
+import categoryType from '~/components/common/CategoryButton/CategoryTypes';
 import useGetDiaryDetail from '~/services/Diary/useGetDiaryDetail';
+import { getTodayDate } from '~/utils/Common';
+
+interface DiaryFormValue {
+  placeName: string;
+  datingDay: string;
+  score: number;
+  category: string;
+  myText: string;
+  opponentText: string;
+  pictures: Pictures;
+}
+
+interface DiaryFormSetMethod {
+  setDatingDay: React.Dispatch<React.SetStateAction<string>>;
+  setScore: React.Dispatch<React.SetStateAction<number>>;
+  setCategory: React.Dispatch<React.SetStateAction<categoryType>>;
+  setMyText: React.Dispatch<React.SetStateAction<string>>;
+  setPictures: React.Dispatch<React.SetStateAction<Pictures>>;
+}
 
 interface DiaryContentContextProps {
   editable: boolean;
@@ -9,6 +29,8 @@ interface DiaryContentContextProps {
   diary: Diary | undefined;
   spotId: string | undefined;
   diaryId: string | undefined;
+  formValue: DiaryFormValue;
+  formSetMethod: DiaryFormSetMethod;
 }
 
 interface DiaryContentProviderParams extends PropsWithChildren {
@@ -26,6 +48,19 @@ const DiaryContentProvider = ({
   );
   const { spotId, diaryId } = params;
   const { data: diary } = useGetDiaryDetail({ diaryId });
+  const placeName = diary?.placeName ?? '';
+  const [datingDay, setDatingDay] = useState<string>(
+    diary?.datingDay ?? getTodayDate(),
+  );
+  const [score, setScore] = useState<number>(diary?.score ?? 5);
+  const [category, setCategory] = useState<categoryType>(
+    diary?.category ?? 'CAFE',
+  );
+  const [myText, setMyText] = useState<string>(diary?.myText ?? '');
+  const opponentText = diary?.opponentText ?? '';
+  const [pictures, setPictures] = useState<Pictures>(
+    diary?.pictures ?? ({} as Pictures),
+  );
 
   return (
     <DiaryContentContext.Provider
@@ -35,6 +70,22 @@ const DiaryContentProvider = ({
         diary,
         spotId,
         diaryId,
+        formValue: {
+          placeName,
+          datingDay,
+          score,
+          category,
+          myText,
+          opponentText,
+          pictures,
+        },
+        formSetMethod: {
+          setDatingDay,
+          setScore,
+          setCategory,
+          setMyText,
+          setPictures,
+        },
       }}
     >
       {children}
