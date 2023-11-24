de90085fd8844d3594a6eef04f83774c7e595a65
Suyeon <msjgr2801@gmail.com>
2023-11-24 18:23:36 +0900

    📝 Docs: diary util 파일 이름 변경

--- src/utils/Diary.ts
+++ src/utils/Diary.ts
@@ -3,6 +3,8 @@ import { Pictures } from '~/types';
 export const changeImageType = (img: Pictures) => {
   const imgArray: string[] = [];
 
+  if (img === undefined || img === null) return imgArray;
+
   Object.entries(img).forEach(([_, value]) => {
     if (value === null) return;
 
