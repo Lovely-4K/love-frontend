f537b7c166257238884ab1662914e85aa10e44e3
cloud0406 <cloud0406@naver.com>
2023-11-25 17:21:20 +0900

    ✨ Feat: diaryHeader 이전 페이지로 이동 처리

--- src/pages/Diary/components/DiaryCommon/DiaryHeader.tsx
+++ src/pages/Diary/components/DiaryCommon/DiaryHeader.tsx
@@ -1,7 +1,9 @@
+import { useNavigate } from 'react-router-dom';
 import { IconTopArrow } from '~/assets/icons';
 import useInfo from '~/pages/Diary/hooks/useInfo';
 
 const DiaryHeader = () => {
+  const navigate = useNavigate();
   const { info } = useInfo();
 
   if (!info) return;
@@ -9,7 +11,10 @@ const DiaryHeader = () => {
   return (
     <div className="font-title flex gap-2 font-bold">
       <button className="text-grey-300">
-        <IconTopArrow className="h-4 w-4 -rotate-90 fill-grey-300" />
+        <IconTopArrow
+          className="h-4 w-4 -rotate-90 fill-grey-300"
+          onClick={() => navigate(-1)}
+        />
       </button>
       <span>{info.content}</span>
     </div>
