688251a336bcc34a3f562ef733dcf61a2fd338a3
Suyeon <msjgr2801@gmail.com>
2023-11-25 22:48:13 +0900

    🔨Refactoring: 다이어리 훅 리팩토링
    - img 관련 훅 추가
    - diary undefined 제어문 삭제
    - locate, params 일괄 관리

--- src/pages/Diary/hooks/DiaryContent/useDiaryContent.ts
+++ src/pages/Diary/hooks/DiaryContent/useDiaryContent.ts
@@ -1,12 +1,12 @@
 import type categoryType from '~/components/common/CategoryButton/CategoryTypes';
-import { ChangeEvent } from 'react';
-import type { Pictures, DiaryResponse, DiaryEditRequest } from '~/types';
+import { ChangeEvent, useEffect, useMemo } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import type { Pictures, DiaryResponse, DiaryCreateTextRequest } from '~/types';
+import { changeImageType } from './../../../../utils/Diary';
 
 interface useDiaryContentParams {
-  diary: DiaryResponse | DiaryEditRequest;
-  setDiary: React.Dispatch<
-    React.SetStateAction<DiaryResponse | DiaryEditRequest>
-  >;
+  diary: DiaryResponse;
+  setDiary: React.Dispatch<React.SetStateAction<DiaryResponse>>;
   editable: boolean;
   setEditable: React.Dispatch<React.SetStateAction<boolean>>;
 }
@@ -16,9 +16,15 @@ const useDiaryContent = ({
   setDiary,
   setEditable,
 }: useDiaryContentParams) => {
-  const handleChangeDatingDay = (event: ChangeEvent<HTMLInputElement>) => {
-    if (!diary) return;
+  const locate = useLocation();
+  const params = useParams();
+  const { placeName, kakaoMapId, latitude, longitude } = useMemo(
+    () => locate.state,
+    [locate],
+  );
+  const { diaryId } = useMemo(() => params, [params]);
 
+  const handleChangeDatingDay = (event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
 
     setDiary({
@@ -28,7 +34,6 @@ const useDiaryContent = ({
   };
 
   const handleChangeMyText = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    if (!diary) return;
     if (event.target instanceof HTMLTextAreaElement) {
       const { value } = event.target;
 
@@ -40,43 +45,124 @@ const useDiaryContent = ({
   };
 
   const handleChangeCategory = (category: categoryType) => {
-    if (!diary) return;
-
     setDiary({
       ...diary,
       category,
     });
   };
 
-  const handleChangePictures = (pictures: Pictures) => {
-    if (!diary) return;
-
+  const handleChangeImgaes = (images: string[]) => {
     setDiary({
       ...diary,
-      pictures,
+      images,
     });
   };
 
   const handleChangeScore = (score: number) => {
-    if (!diary) return;
-
     setDiary({
       ...diary,
       score,
     });
   };
 
+  const changePictureToImage = (pictures: Pictures) => {
+    const images = changeImageType(pictures);
+
+    setDiary({
+      ...diary,
+      images,
+    });
+  };
+
+  const handleChangeFileList = (fileList: FileList) => {
+    setDiary({
+      ...diary,
+      files: fileList,
+    });
+  };
+
+  const handleAddImages = (event: ChangeEvent<HTMLInputElement>) => {
+    if (diary.images === undefined) return;
+
+    const fileLists = event.target.files!;
+    const images = [...diary.images];
+    if (images.length > 5) {
+      alert('사진은 최대 5개까지 첨부가능해요!');
+
+      return;
+    }
+
+    for (let i = 0; i < fileLists.length; i++) {
+      const currentImageUrl = URL.createObjectURL(fileLists[i]);
+      images.push(currentImageUrl);
+    }
+
+    handleChangeFileList(fileLists);
+    handleChangeImgaes(images);
+  };
+
+  const handleDeleteImage = (id: number) => {
+    if (diary.images === undefined) return;
+    const images = [...diary.images].filter((_, index) => index !== id);
+
+    handleChangeImgaes(images);
+  };
+
   const handleEditMode = () => {
     setEditable(true);
   };
 
+  const handleSubmitCreate = () => {
+    const formData = new FormData();
+    const { datingDay, category, score, files, myText } = diary;
+    const texts: DiaryCreateTextRequest = {
+      placeName,
+      kakaoMapId,
+      latitude,
+      longitude,
+      datingDay,
+      category,
+      score,
+      text: myText,
+    };
+    formData.append('text', JSON.stringify(texts));
+    if (files) {
+      for (const file of files) {
+        formData.append('images', file);
+      }
+    }
+
+    //! create API 요청
+  };
+
+  const handleSubmitEdit = () => {
+    const formData = new FormData();
+    const { datingDay, category, myText, opponentText, score } = diary;
+    const texts = JSON.stringify({
+      datingDay,
+      category,
+      myText,
+      opponentText,
+      score,
+    });
+    formData.append('text', texts);
+  };
+
   const handleSubmitForm = () => {
-    setEditable(false);
+    if (diaryId === undefined) {
+      handleSubmitCreate();
+    } else {
+      handleSubmitEdit();
+    }
   };
 
   const handleEditCancel = () => {
     setEditable(false);
   };
+
+  useEffect(() => {
+    changePictureToImage(diary.pictures);
+  }, []);
 
   return {
     handleEditCancel,
@@ -86,7 +172,9 @@ const useDiaryContent = ({
     handleChangeMyText,
     handleChangeScore,
     handleChangeCategory,
-    handleChangePictures,
+    handleChangeImgaes,
+    handleAddImages,
+    handleDeleteImage,
   };
 };
 
