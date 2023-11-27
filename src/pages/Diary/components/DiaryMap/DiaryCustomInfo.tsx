dc35bee558770a9bc79e332d9c0e2065cdf49060
cloud0406 <cloud0406@naver.com>
2023-11-27 13:42:43 +0900

    📝 Chore: 빌드 오류 해결

--- src/pages/Diary/components/DiaryMap/DiaryCustomInfo.tsx
+++ src/pages/Diary/components/DiaryMap/DiaryCustomInfo.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { CustomOverlayMap } from 'react-kakao-maps-sdk';
 import { MapMarker } from '~/types';
 import DiaryMarkerData from '~/pages/Diary/components/DiaryCommon/DiaryMarkerData';
@@ -7,8 +8,21 @@ interface DiaryCustomInfoProps {
 }
 
 const DiaryCustomInfo = ({ info }: DiaryCustomInfoProps) => {
+  useEffect(() => {
+    const customOverlayElement = document.querySelector(
+      `#customOverlay-${info.spotId}`,
+    );
+
+    if (customOverlayElement && customOverlayElement.parentElement) {
+      customOverlayElement.parentElement.style.zIndex = '999'; // 원하는 z-index 값으로 수정
+    }
+  }, [info.spotId]);
+
   return (
-    <CustomOverlayMap position={info.position} id={info.spotId}>
+    <CustomOverlayMap
+      position={info.position}
+      id={`customOverlay-${info.spotId}`}
+    >
       <div className="absolute -right-24 bottom-[2.75rem] z-30 min-w-[12rem] rounded-xl bg-base-white p-4 drop-shadow-xl">
         <DiaryMarkerData content={info.content} address={info.address} />
       </div>
