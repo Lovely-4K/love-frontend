974c3f3c046cb2cbb8d43881ed722c64a5b4e7ac
cloud0406 <cloud0406@naver.com>
2023-11-29 16:35:12 +0900

    📝 Chore: 불필요 import 구문 제거

--- src/pages/Diary/hooks/Diary/useMapLocation.ts
+++ src/pages/Diary/hooks/Diary/useMapLocation.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { MapMarker } from '~/types';
 import { DiaryContextProps } from '~/pages/Diary/contexts/DiaryContext';
 
