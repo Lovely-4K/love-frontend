e9128b850994c45daa31661824750d7c73a0173e
Suyeon <msjgr2801@gmail.com>
2023-12-26 17:19:58 +0900

    ✨ Feat: 에러 표시 사항 변경

--- src/pages/Question/components/QuestionErrorFallback.tsx
+++ src/pages/Question/components/QuestionErrorFallback.tsx
@@ -2,12 +2,14 @@ import { FallbackProps } from 'react-error-boundary';
 import { Button } from '~/components/common';
 
 const SideBarFallBack = ({ error, resetErrorBoundary }: FallbackProps) => {
+  console.error(error.message);
+
   return (
     <div className="flex h-full w-full flex-col items-center justify-center">
       <div className="flex flex-col justify-center">
         <div className="w-full text-center text-2xl">😢</div>
         <div className="w-full p-4 text-center text-grey-400">
-          {error.message}
+          질문을 받아오지 못했어요. 다시 시도해보시겠어요?
         </div>
       </div>
       <div>
