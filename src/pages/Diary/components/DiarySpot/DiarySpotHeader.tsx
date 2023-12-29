a6919dc7162684ad7e1cb328bb88f26def377434
cloud0406 <cloud0406@naver.com>
2023-12-29 13:52:46 +0900

    🔨 Refactor: DiarySpot jotai로 변환

--- src/pages/Diary/components/DiarySpot/DiarySpotHeader.tsx
+++ src/pages/Diary/components/DiarySpot/DiarySpotHeader.tsx
@@ -1,15 +1,27 @@
 // import useDiaryContext from '../../hooks/Diary/useDiaryContext';
 import { useAtomValue } from 'jotai';
-import useDiarySpotContext from '../../hooks/useDiarySpotContext';
+import { useParams } from 'react-router-dom';
 import { Button } from '~/components/common';
 import { DiaryHeader } from '~/pages/Diary/components/DiaryCommon';
+import useDiarySpot from '~/pages/Diary/hooks/DiarySpot/useDiarySpot';
+import useGetSpotDiarys from '~/services/diary/useGetSpotDiarys';
 import { infoAtom } from '~/stores/diaryAtoms';
+import { deleteModeAtom } from '~/stores/diarySpotAtoms';
 
 const DiarySpotHeader = () => {
-  const diarySpotContext = useDiarySpotContext();
-  const { spotDiaries, deleteMode, handleDeleteMode } = diarySpotContext;
+  const params = useParams();
+  const deleteMode = useAtomValue(deleteModeAtom);
   const info = useAtomValue(infoAtom);
+  const { handleDeleteMode } = useDiarySpot();
   const deleteButtonLabel = deleteMode ? '삭제하기' : '선택 삭제';
+
+  if (info === undefined || info.spotId !== params.spotId) {
+    throw new Error('앗! 유효하지 않은 접근이에요!');
+  }
+
+  const { data: spotDiaries } = useGetSpotDiarys({
+    kakaoMapId: Number(info.spotId),
+  });
 
   const deleteButtonStyle = deleteMode
     ? 'bg-base-primary text-base-white'
